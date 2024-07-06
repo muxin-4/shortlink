8e49408c8c9acf9727ad2887da2f468a34d04872
muxin <wgy952046097@gmail.com>
2024-07-06 17:29:29 +0800

    feat: 查询用户信息功能

--- admin/src/main/java/com/yaya/shortlink/admin/ShortLinkAdminApllication.java
+++ admin/src/main/java/com/yaya/shortlink/admin/ShortLinkAdminApllication.java
@@ -1,9 +1,11 @@
 package com.yaya.shortlink.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yaya.shortlink.admin.dao.mapper")
 public class ShortLinkAdminApllication {
 
     public static void main(String[] args) {
