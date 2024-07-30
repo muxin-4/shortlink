b4c4a0df193c05c36f39637ae1962bd8a681f7a4
muxin-4 <952046097@qq.com>
2024-07-30 22:54:01 +0800

    feat: 开发短链接后管系统短链接分组绑定用户上下文

---
+++ admin/src/main/java/com/yaya/shortlink/admin/config/UserConfiguration.java
@@ -0,0 +1,42 @@
+package com.yaya.shortlink.admin.config;/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import com.yaya.shortlink.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * 用户配置自动装配
+ */
+@Configuration
+public class UserConfiguration {
+
+    /**
+     * 用户信息传递过滤器
+     */
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter(StringRedisTemplate stringRedisTemplate) {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(0);
+        return registration;
+    }
+}
