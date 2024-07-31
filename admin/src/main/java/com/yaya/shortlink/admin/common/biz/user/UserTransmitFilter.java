7b9236b63a47ac423c905096096bdc7e913297b3
muxin-4 <952046097@qq.com>
2024-07-31 21:47:17 +0800

    feat: 开发短链接后管系统短链接分组绑定用户上下文

--- admin/src/main/java/com/yaya/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/yaya/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -26,6 +26,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.Objects;
+
 /**
  * 用户信息传输过滤器
  */
@@ -37,13 +39,15 @@ public class UserTransmitFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
-        Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-
-        if (userInfoJsonStr != null) {
-            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-            UserContext.setUser(userInfoDTO);
+        String requestURI = httpServletRequest.getRequestURI();
+        if(!Objects.equals(requestURI, "/api/short-link/v1/user/login")) {
+            String username = httpServletRequest.getHeader("username");
+            String token = httpServletRequest.getHeader("token");
+            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+            if (userInfoJsonStr != null) {
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+            }
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
