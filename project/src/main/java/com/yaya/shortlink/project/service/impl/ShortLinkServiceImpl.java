5bb725d2e0c22289cd0949f62effa3a8d99c3078
muxin <wgy952046097@gmail.com>
2024-11-14 18:13:15 +0800

    feat: 新增短链接（下）1

--- project/src/main/java/com/yaya/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/yaya/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -1,6 +1,8 @@
 package com.yaya.shortlink.project.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yaya.shortlink.project.common.convention.exception.ServiceException;
 import com.yaya.shortlink.project.dao.entity.ShortLinkDO;
@@ -33,14 +35,18 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
         shortLinkDO.setShortUri(shortLinkSuffix);
         shortLinkDO.setEnableStatus(0);
         shortLinkDO.setFullShortUrl(fullShortUrl);
-        try{
+        try {
             baseMapper.insert(shortLinkDO);
-        }catch (DuplicateKeyException e) {
+        } catch (DuplicateKeyException e) {
             // TODO 已经误判的短链接如何处理
             // 第一种，短链接确实真实存在缓存
             // 第二种，短链接不一定存在缓存中
-            log.warn("短链接：{} 重复入库");
-            throw new ServiceException("短链接生成重复");
+            LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class).eq(ShortLinkDO::getFullShortUrl, fullShortUrl);
+            ShortLinkDO hasShortLinkDO = baseMapper.selectOne(queryWrapper);
+            if(hasShortLinkDO != null) {
+                log.warn("短链接：{} 重复入库");
+                throw new ServiceException("短链接生成重复");
+            }
         }
         shortUriCreateCachePenetrationBloomFilter.add(shortLinkSuffix);
         return ShortLinkCreateRespDTO.builder()
@@ -58,6 +64,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 throw new ServiceException("短连接频繁生成，请稍后再试");
             }
             String originUrl = requestParams.getOriginUrl();
+            originUrl += System.currentTimeMillis();
             shortUri = HashUtil.hashToBase62(originUrl);
             if (!shortUriCreateCachePenetrationBloomFilter.contains(requestParams.getDomain() + "/" + shortUri)) {
                 break;
